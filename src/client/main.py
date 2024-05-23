7c057e1d1fa16ef6025ab6ef3468d4714dfa418c
v.heiduck96 <v.heiduck@campus.tu-berlin.de>
2024-05-23 19:14:36 +0200

    Text-Output statt Audio-Output

--- src/client/main.py
+++ src/client/main.py
@@ -2,8 +2,7 @@ from time import time
 import logging
 from requests import post
 from os import path
-from platform import system
-from subprocess import run, CalledProcessError
+import webbrowser
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
@@ -21,33 +20,6 @@ def log_response(response, total):
     logging.info(f"Total delay of {total}s: Whisper took {whisper}s and TTS took {tts}s")
 
 
-def play_audio(audio_path):
-    """
-    This function is supposed to play audio given a path, but has only been tested on Raspbian, Darwin and Windows.
-    """
-    if audio_path is None:
-        logging.error("Error in play_audio: No audio path provided")
-
-    os_name = system()
-    if os_name == "Linux":
-        try:
-            run(["mpg321", "-q", audio_path], check=True)
-        except CalledProcessError as e:
-            logging.error(f"Error playing audio on Linux: {e}")
-    elif os_name == "Windows":
-        try:
-            run(["cmd.exe", "/c", "start", "/wait", audio_path], check=True)
-        except CalledProcessError as e:
-            logging.error(f"Error playing out on Windows: {e}")
-    elif os_name == "Darwin":
-        try:
-            run(['afplay', audio_path], check=True)
-        except CalledProcessError as e:
-            logging.error(f"Error playing audio on macOS: {e}")
-    else:
-        raise OSError(f"Unsupported operation system: {os_name}")
-
-
 def remote_whisper(input_file_path):
     """
     sends local audio file to server, saves response and returns its location.
@@ -61,10 +33,10 @@ def remote_whisper(input_file_path):
         if response.status_code == 200:
             language = response.headers.get("X-Language")
             log_response(response, round(total_time, 3))
-            output_file_path = get_path("audio_files", "output.mp3")
+            output_file_path = get_path("transcripts", "transcript.txt")
             print("Pfad:" + output_file_path)
-            with open(output_file_path, "wb") as f:
-                f.write(response.content)
+            with open(output_file_path, "wb") as fi:
+                fi.write(response.content)
             return output_file_path, language
         else:
             logging.error(f"Server responded with status code: {response.status_code}")
@@ -77,4 +49,4 @@ def remote_whisper(input_file_path):
 if __name__ == "__main__":
     # currently just takes a local input.mp3 and plays response (output.mp3)
     file_path, language = remote_whisper(get_path("audio_files", "input.mp3"))
-    play_audio(file_path)
+    webbrowser.open(file_path)
--- src/server/service.py
+++ src/server/service.py
@@ -3,8 +3,6 @@ from os import path, remove
 from time import time
 from warnings import filterwarnings
 from whisper import load_model
-from langdetect import detect
-from gtts import gTTS
 from fastapi import FastAPI, UploadFile
 from pydantic import BaseModel
 from starlette.middleware.cors import CORSMiddleware
@@ -13,18 +11,16 @@ from starlette.responses import RedirectResponse, FileResponse
 # define model size (tiny, base, medium, large)
 WHISPER_MODEL = "tiny"
 
-
 # Configure logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
-
 
 # Suppress specific warnings (FP16)
 filterwarnings("ignore", category=UserWarning, message="FP16 is not supported on CPU; using FP32 instead")
 
 logging.info("Loading finished - starting server")
 app = FastAPI(
-    title="Whisper API", 
-    description="""Whisper API""", 
+    title="Whisper API",
+    description="""Whisper API""",
     version="0.1"
 )
 origins = ["*"]
@@ -41,26 +37,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class Query(BaseModel):
     query: str
 
+
 logging.info("All complete - ready for traffic")
 
-# Google Text-to-Speech 
-def save_tts_gtts(response: str):
-    if response == None: 
-        return
-    try:
-        file_path = path.join(path.dirname(path.abspath(__file__)), "audio_files", "output.wav")
-        tts = gTTS(response, lang=detect(response)) # detect used in case multi-lingual interaction is desired
-        tts.save(file_path)
-        return file_path
-    except Exception as e:
-        logging.error(f"Error in save_tts: {e}")
 
 @app.get("/")
-def swagger_documentaiton():
+def swagger_documentation():
     return RedirectResponse(url='/docs')
+
 
 @app.post("/whisper")
 async def whisper(file: UploadFile):
@@ -71,20 +59,21 @@ async def whisper(file: UploadFile):
 
     # transcribe audio file
     current_time = time()
-    transcription_result = model.transcribe(temp_filename) 
+    transcription_result = model.transcribe(temp_filename)
     whisper_time = time() - current_time
     remove(temp_filename)
     print(f"Transcription result: {transcription_result['text']}")
+    file_path = path.join(path.dirname(path.abspath(__file__)), "transcription.txt")
+    with open(file_path, "w", encoding="utf-8") as txt:
+        txt.write(transcription_result["text"])
 
-    # generate audio output
     current_time = time()
-    audio_file_path = save_tts_gtts(transcription_result["text"])
     tts_time = time() - current_time
 
     # send response with meta-data in headers
     return FileResponse(
-        path=audio_file_path, 
-        media_type="audio/wav", 
+        path=file_path,
+        media_type="text/plain",
         headers={
             "X-Language": transcription_result["language"],
             "Time-Whisper": str(whisper_time),
